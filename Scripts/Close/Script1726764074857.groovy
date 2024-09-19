6581a896dad13443c5175259c98b6d8694343bb8
Rohit Roy <Rohit Roy@DESKTOP-40FUI3F>
2024-09-19 22:13:04 +0530

    2nd commit

--- Scripts/Close/Script1726764074857.groovy
+++ Scripts/Close/Script1726764074857.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.closeBrowser()
 
-WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+WebUI.callTestCase(findTestCase('Open Browser'), [:], FailureHandling.STOP_ON_FAILURE)
 
