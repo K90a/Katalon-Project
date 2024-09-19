1a76ba2e1c6d2bd0906dd8d9f935a1893247640c
Rohit Roy <Rohit Roy@DESKTOP-40FUI3F>
2024-09-19 20:58:05 +0530

    1st Demo Commit

---
+++ Test Suites/Check.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Check</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9e66a52d-c544-461c-b5fb-2250996aaa3e</testSuiteGuid>
+   <testCaseLink>
+      <guid>0c09e2dc-4c62-447a-8054-e5ec7e4f17ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Open Browser</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
