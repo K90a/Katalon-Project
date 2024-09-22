404935b9614d512b03de0ca3e44707185f61b0ad
Rohit Roy <Rohit Roy@DESKTOP-40FUI3F>
2024-09-22 15:21:37 +0530

    11th commit

---
+++ Test Suites/Check Test Object.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Check Test Object</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2693fbdf-697a-4a0f-8de8-0f42e468e13e</testSuiteGuid>
+   <testCaseLink>
+      <guid>893997c6-a9cb-40b0-9814-09a5b870726b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Object</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
