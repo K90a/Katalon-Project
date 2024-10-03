24c6bb85e1d16a523d7f091750b9cc2569a025c9
Rohit Roy <Rohit Roy@DESKTOP-40FUI3F>
2024-10-03 10:01:26 +0530

    31th commit

---
+++ Test Suites/interaction.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>interaction</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>733a9370-65ce-46da-b587-bfa5b12b809d</testSuiteGuid>
+   <testCaseLink>
+      <guid>5c1ad7d1-21fd-4bc5-9eb8-de67f2d70163</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Simpler interaction</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
