409c22055cfb450e376754af9355551439fd1d5e
Rohit Roy <Rohit Roy@DESKTOP-40FUI3F>
2024-10-03 14:35:24 +0530

    35th commit

--- Test Suites/Test Data.ts
+++ Test Suites/Test Data.ts
@@ -5,10 +5,38 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>29077499-afa0-40c0-92a0-b02ad691185d</testSuiteGuid>
+   <testCaseLink>
+      <guid>ae78e610-e2bc-4ba4-a0f0-b974a3d65d35</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Find a Place</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>0200ae0d-526c-4764-ad30-7f1d4abda6ee</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Search Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>0200ae0d-526c-4764-ad30-7f1d4abda6ee</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>City</value>
+         <variableId>bc5eb321-157a-4ff5-88cf-c135c7c05314</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>0200ae0d-526c-4764-ad30-7f1d4abda6ee</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>ExpectedResult</value>
+         <variableId>6870c9e9-9909-4292-b993-0c912471da2f</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
