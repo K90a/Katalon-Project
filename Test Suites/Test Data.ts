b5bf39458ab6344825326e674575b7a04a129f12
Rohit Roy <Rohit Roy@DESKTOP-40FUI3F>
2024-10-03 14:29:42 +0530

    34th b commit

---
+++ Test Suites/Test Data.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Data</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>29077499-afa0-40c0-92a0-b02ad691185d</testSuiteGuid>
+</TestSuiteEntity>
